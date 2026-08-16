f92a054d6a2bb0ac22b737af0cfb22138bd5ac69
Amar Khadka <amarkhadkabardiya1234@gmail.com>
2026-08-16 21:39:12 +0545

    completed filter panel

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import type { Product } from "../types";
 import { Link, useSearchParams } from "react-router-dom";
 import { categoriesData, dummyProducts } from "../assets/assets";
-import { ChevronDown, Home, SlidersHorizontal } from "lucide-react";
+import { ChevronDown, Home, SlidersHorizontal, XIcon } from "lucide-react";
 import ProductCard from "../components/Home/ProductCard";
 import Loading from "../components/Loading";
+import FilterPanel from "../components/FilterPanel";
 
 
 const Products = () => {
@@ -29,7 +30,7 @@ const Products = () => {
     setLoading(false)
   }
 
-  const updateFilter = (key: string, value: string)=> (key: string, value: string)=> {
+  const updateFilter = (key: string, value: string)=>{
     const newParams = new URLSearchParams(searchParams)
     if(value) {
       newParams.set(key, value)
@@ -69,7 +70,10 @@ const Products = () => {
           {/* Sidebar - Desktop */}
           <aside className="hidden lg:block w-64 shrink-0">
             <div className="bg-white rounded-2xl p-4 sticky top-24">
-              <p>Filter</p>
+              <FilterPanel categories={categoriesData} category={category} organic={organic} minPrice={minPrice}
+              maxPrice={maxPrice} updateFilter={updateFilter} clearFilters={clearFilters} hasFilters={hashFilters
+
+              } />
             </div>
           </aside>
 
@@ -140,6 +144,29 @@ const Products = () => {
         </div>
       </div>
 
+
+      {/* Mobile Filters Modal */}
+       
+
+       {mobileFiltersOpen &&  (
+        <>
+        <div className="fixed inset-0 bg-black/40 z-50" onClick={()=>
+          setMobileFiltersOpen(false)} />
+
+          <div className="fixed bottom-0 left-0 right-0 bg-white z-50 rounded-t-2xl max-h-[80vh] overflow-y-auto animate-slide-in-up">
+            
+        <div className="flex items-center justify-between p-4 border-b border-app-border">
+
+          <h3 className="text-lg font-semibold text-app-green">Filters</h3>
+          <button onClick={()=> {console.log("clicked"); setMobileFiltersOpen(false)} } className="p-2 hover:bg-app-cream rounded-lg">
+
+            <XIcon className="size-5"/>
+          </button>
+        </div>
+            
+          </div>
+        </>
+       )}
     </div>
   )
 }
