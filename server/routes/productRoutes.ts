e0e692a906d57e2645222b7f82af7b82f09e2ff4
Amar Khadka <amarkhadkabardiya1234@gmail.com>
2026-09-01 07:58:52 +0545

    compeleted api for products

---
+++ server/routes/productRoutes.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { createProduct, deleteProduct, getFalshDeals, getProduct, getProducts, updateProduct } from "../controllers/productController.js";
+import auth from "../middleware/auth.js";
+import admin from "../middleware/admin.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/flash-deals", getFalshDeals);
+productRouter.get("/", getProducts);
+productRouter.get("/:id", getProduct);
+productRouter.post("/", auth, admin, createProduct);
+productRouter.put("/:id", auth, admin, updateProduct);
+productRouter.delete("/:id", auth, admin, deleteProduct);
+
+export default productRouter;
