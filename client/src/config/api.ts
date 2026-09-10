2d419e4b3fa30e2eaba1628e1ce9d3bb41e18ab3
Amar Khadka <amarkhadkabardiya1234@gmail.com>
2026-09-10 20:36:47 +0545

    created api in the client side

---
+++ client/src/config/api.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+})
+
+// Inject JWT token from localStorage into evry request
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("auth_token")
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+})
+
+// Hanle auth errors globlly
+api.interceptors.request.use(
+    (response)=> response,
+    (error)=> {
+        if(error.response?.status === 401){
+            localStorage.removeItem("auth_token");
+            localStorage.removeItem("auth_user");
+            // Only redirect if not already on atuh pages
+            if(!window.location.pathname.includes("/login") && 
+        !window.location.pathname.includes("/register")){
+            window.location.href ="/login"
+        }
+        }
+
+        return Promise.reject(error)
+    }
+)
+
+export default api
