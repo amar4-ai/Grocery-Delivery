4fb27f5415249d8b0ba95578983abbebc1544f7a
Amar Khadka <amarkhadkabardiya1234@gmail.com>
2026-09-21 23:18:54 +0545

    project completed

--- server/server.ts
+++ server/server.ts
@@ -10,11 +10,18 @@ import { inngest, functions } from "./inngest/index.js"
 import addressRouter from "./routes/addressRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import deliveryPartnerRouter from "./routes/deliveryPartnerRoutes.js";
+import { stripeWebhook } from "./controllers/webhooks.js";
+
+
+
 const app = express();
+app.post("/api/stripe", express.raw({type: 'application/json'}), stripeWebhook)
 
 // Middleware
 app.use(cors())
 app.use(express.json());
+
+
 
 const port = process.env.PORT || 5000;
 
